7219fc1c3cb2fdf57f6ec8631ea88af6bafddfe7
snipe <snipe@snipe.net>
2025-12-02 16:22:53 +0000

    Added style to border on colorpicker

--- resources/views/blade/input/colorpicker.blade.php
+++ resources/views/blade/input/colorpicker.blade.php
@@ -9,7 +9,7 @@
 <!-- Colorpicker -->
 <div {{ $attributes->merge(['class' => 'color input-group colorpicker-component row col-md-5']) }} id="{{ $div_id }}">
     <input class="form-control" placeholder="{{ $placeholder }}" aria-label="{{ $name }}" name="{{ $name }}" type="text" id="{{ $id }}" value="{{ old($name, ($item->{$name} ?? '')) }}" {{ (config('app.lock_passwords')===true) ? ' disabled' : '' }}>
-    <span class="input-group-addon"><i> </i></span>
+    <span class="input-group-addon"><i style="border: 1px solid #b8b6b6"> </i></span>
 </div>
 
 @if (config('app.lock_passwords')===true)
