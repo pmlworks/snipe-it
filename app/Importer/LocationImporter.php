4f610ac1af24d6b08b3ee1fdda4578bf162e7682
snipe <snipe@snipe.net>
2025-12-04 11:06:50 +0000

    Added tag color to importer

--- app/Importer/LocationImporter.php
+++ app/Importer/LocationImporter.php
@@ -75,6 +75,7 @@ class LocationImporter extends ItemImporter
         $this->item['manager'] = trim($this->findCsvMatch($row, 'manager'));
         $this->item['manager_username'] = trim($this->findCsvMatch($row, 'manager_username'));
         $this->item['notes'] = trim($this->findCsvMatch($row, 'notes'));
+        $this->item['tag_color'] = trim($this->findCsvMatch($row, 'tag_color'));
 
 
         if ($this->findCsvMatch($row, 'parent_location')) {
