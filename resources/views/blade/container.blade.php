3bc5ab593a04170053a21e59f53d9956fd4623fb
snipe <snipe@snipe.net>
2026-01-20 14:49:09 +0000

    Next step in container+box component

--- resources/views/blade/container.blade.php
+++ resources/views/blade/container.blade.php
@@ -1,10 +1,21 @@
 @props([
-'class' => 'col-md-12',
+    'class' => 'col-md-12',
+    'columns' => 1,
 ])
 
 <!-- Start container+row component -->
-<div class="row">
-    <div class="{{ $class }}">
+<div {{ $attributes->merge(['class' => 'row']) }}>
+
+    <!-- Only one column, so set the general col-md-12 div -->
+    @if ($columns == 1)
+        <x-page-column class="{{ $class }}">
+            {{ $slot }}
+        </x-page-column>
+
+    @else
+        <!-- the page using this should specify column names via the page-column component -->
         {{ $slot }}
-    </div>
+    @endif
+
+
 </div>
