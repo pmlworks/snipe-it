2048eb7d5ab73fa78131fb17fb529000e86e0abe
snipe <snipe@snipe.net>
2026-01-23 17:08:25 +0000

    Removed unneeded presenter methods

--- app/Presenters/GroupPresenter.php
+++ app/Presenters/GroupPresenter.php
@@ -74,51 +74,5 @@ class GroupPresenter extends Presenter
 
         return json_encode($layout);
     }
-
-
-    /**
-     * Link to this supplier name
-     * @return string
-     */
-    public function nameUrl()
-    {
-        return (string) link_to_route('suppliers.show', $this->name, $this->id);
-    }
-
-    /**
-     * Getter for Polymorphism.
-     * @return mixed
-     */
-    public function name()
-    {
-        return $this->model->name;
-    }
-
-    /**
-     * Url to view this item.
-     * @return string
-     */
-    public function viewUrl()
-    {
-        return route('suppliers.show', $this->id);
-    }
-
-    public function glyph()
-    {
-        return '<x-icon type="suppliers" />';
-    }
-
-    public function fullName()
-    {
-        return $this->name;
-    }
-
-    public function formattedNameLink() {
-
-        if (auth()->user()->can('view', ['\App\Models\Supplier', $this])) {
-            return ($this->tag_color ? "<i class='fa-solid fa-fw fa-square' style='color: ".e($this->tag_color)."' aria-hidden='true'></i>" : '').'<a href="'.route('suppliers.show', e($this->id)).'">'.e($this->name).'</a>';
-        }
-
-        return ($this->tag_color ? "<i class='fa-solid fa-fw fa-square' style='color: ".e($this->tag_color)."' aria-hidden='true'></i> " : '').$this->name;
-    }
+    
 }
