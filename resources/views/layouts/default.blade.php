d5997e2394d3049dcd38e7c9ba51dbcf8717f728
snipe <snipe@snipe.net>
2026-01-24 17:40:29 +0000

    Added new classes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -122,10 +122,15 @@
         .label2_fields,
         .l2fd-main,
         .l2fd-listitem,
-        .fixed-table-loading
+        .fixed-table-loading,
+        .list-group-item
         {
             background-color: var(--box-bg) !important;
             color: var(--color-fg) !important;
+        }
+
+        .list-group-item {
+            border: var(--tab-bottom-border);
         }
 
         footer.main-footer {
@@ -419,6 +424,8 @@
         .box.box-default {
             border-top:  var(--box-header-top-border);
         }
+
+
 
         .box-header.with-border {
             border-bottom: var(--box-header-bottom-border);
@@ -893,6 +900,10 @@
 
         .small-box h3, .small-box p {
             color: white !important;
+        }
+
+        .box.box-theme {
+            border-top:  var(--main-theme-color) !important;
         }
 
     </style>
