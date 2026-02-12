00408f01036b51aab536f59ecc60c4fbf8fb24e2
Marcus Moore <contact@marcusmoore.io>
2026-02-11 16:06:27 -0800

    Revert "Put checkout_qty on checkoutable so accessory and consumable emails are formatted correctly"
    This reverts commit 548cceee184d97cf343ca5ee744a75733963dcef.

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -1212,8 +1212,6 @@ class ReportsController extends Controller
         if (is_null($email) || $email === '') {
             return redirect()->route('reports/unaccepted_assets')->with('error', trans('general.no_email'));
         }
-
-        $acceptance->checkoutable->checkout_qty = 1;
         $mailable = $this->getCheckoutMailType($acceptance, $logItem);
         Mail::to($email)->send($mailable->locale($locale));
 
