1b7fe4f7284120cd0ae14b87ac37c05bba9ebe73
Marcus Moore <contact@marcusmoore.io>
2026-03-03 11:07:28 -0800

    Scaffold test cases

---
+++ tests/Feature/Assets/Ui/BulkAuditAssetsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Assets\Ui;
+
+use Tests\TestCase;
+
+class BulkAuditAssetsTest extends TestCase
+{
+    public function test_permission_required_to_view_page()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_permission_required_to_bulk_audit_assets()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_can_bulk_audit_assets()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @link https://github.com/grokability/snipe-it/issues/18495
+     */
+    public function test_next_audit_date_is_correctly_updated()
+    {
+        $this->markTestIncomplete();
+    }
+}
