2e7046a810ce1f7562dec9d3ee4fee0cbc7262db
snipe <snipe@snipe.net>
2026-03-13 16:32:11 +0000

    Apply pint to Exceptions directory

--- app/Exceptions/ItemStillHasAccessories.php
+++ app/Exceptions/ItemStillHasAccessories.php
@@ -2,8 +2,6 @@
 
 namespace App\Exceptions;
 
-use Exception;
-
 class ItemStillHasAccessories extends ItemStillHasChildren
 {
     //
--- app/Exceptions/ItemStillHasChildren.php
+++ app/Exceptions/ItemStillHasChildren.php
@@ -6,9 +6,9 @@ use Exception;
 
 class ItemStillHasChildren extends Exception
 {
-    //public function __construct($message, $code = 0, Exception $previous = null, $parent, $children)
-    //{
+    // public function __construct($message, $code = 0, Exception $previous = null, $parent, $children)
+    // {
     //    trans()
     //
-    //}
+    // }
 }
