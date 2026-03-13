97854ad02da004daae87066d489518ca96aab2a2
snipe <snipe@snipe.net>
2026-03-13 18:27:23 +0000

    Bumped hash

--- config/version.php
+++ config/version.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'app_version' => 'v8.4.0',
-    'full_app_version' => 'v8.4.0 - build 21280-g91a95dbc6',
-    'build_version' => '21280',
-    'prerelease_version' => '',
-    'hash_version' => 'g91a95dbc6',
-    'full_hash' => 'v8.4.0-475-g91a95dbc6',
-    'branch' => 'develop',
-];
+return array (
+  'app_version' => 'v8.4.0',
+  'full_app_version' => 'v8.4.0 - build 21690-g20382ea5bf',
+  'build_version' => '21690',
+  'prerelease_version' => '',
+  'hash_version' => 'g20382ea5bf',
+  'full_hash' => 'v8.4.0-408-g20382ea5bf',
+  'branch' => 'master',
+);
