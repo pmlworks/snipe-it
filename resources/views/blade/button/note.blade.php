e1809f1c1997e04f617810e2e715940b1ad5f0e6
snipe <snipe@snipe.net>
2026-03-20 18:21:05 +0000

    Only allow notes, labels if not deleted

--- resources/views/blade/button/note.blade.php
+++ resources/views/blade/button/note.blade.php
@@ -3,16 +3,17 @@
     'route' => null,
     'wide' => false,
 ])
-
-@can('update', $item)
-    <!-- start note button component -->
-    <a href="#" data-toggle="modal" data-target="#createNoteModal" class="btn btn-sm btn-theme hidden-print{{ ($wide=='true') ?? ' btn-block btn-social'  }}" data-tooltip="true" data-placement="top" data-title="{{ trans('general.add_note') }}">
-        <x-icon type="note" class="fa-fw"/>
+@if ($item->deleted_at=='')
+    @can('update', $item)
+        <!-- start note button component -->
+        <a href="#" data-toggle="modal" data-target="#createNoteModal" class="btn btn-sm btn-theme hidden-print{{ ($wide=='true') ?? ' btn-block btn-social'  }}" data-tooltip="true" data-placement="top" data-title="{{ trans('general.add_note') }}">
+            <x-icon type="note" class="fa-fw"/>
 
             @if ($wide=='true')
-            {{ trans('general.add_note') }}
+                {{ trans('general.add_note') }}
             @endif
 
         </a>
-    <!-- end note button component -->
-@endcan
+        <!-- end note button component -->
+    @endcan
+@endif
