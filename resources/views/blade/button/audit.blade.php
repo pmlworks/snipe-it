afbd31ca01945a81ed55fc70063a218ebd7219cd
snipe <snipe@snipe.net>
2026-03-20 18:22:53 +0000

    Only allow audit if not deleted

--- resources/views/blade/button/audit.blade.php
+++ resources/views/blade/button/audit.blade.php
@@ -5,12 +5,14 @@
 ])
 
 @can('audit', $item)
-    <span class="tooltip-wrapper"{!! (!$item->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid_fix').'"' : '') !!}>
-        <a href="{{ $route  }}" class="btn btn-sm btn-primary hidden-print{{ (!$item->model ? ' disabled' : '') }}" data-tooltip="true" title="{{ trans('general.audit') }}">
-             <x-icon type="audit" class="fa-fw"/>
-             @if ($wide=='true')
-                {{ trans('general.audit') }}
-            @endif
-        </a>
-    </span>
+    @if ($item->deleted_at=='')
+        <span class="tooltip-wrapper"{!! (!$item->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid_fix').'"' : '') !!}>
+            <a href="{{ $route  }}" class="btn btn-sm btn-primary hidden-print{{ (!$item->model ? ' disabled' : '') }}" data-tooltip="true" title="{{ trans('general.audit') }}">
+                 <x-icon type="audit" class="fa-fw"/>
+                 @if ($wide=='true')
+                    {{ trans('general.audit') }}
+                @endif
+            </a>
+        </span>
+    @endif
 @endcan
