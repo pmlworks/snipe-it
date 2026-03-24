d02278930d482c1147d81d9e91d97f617c07696d
snipe <snipe@snipe.net>
2026-03-24 12:39:16 +0000

    Pass an alignment to the dl

--- resources/views/blade/data-row.blade.php
+++ resources/views/blade/data-row.blade.php
@@ -3,6 +3,7 @@
     'copy_what' => null,
     'icon_type' => null,
     'hide_if_null' => false,
+    'align' => 'left',
 ])
 
 @if ($hide_if_null!='true')
@@ -12,7 +13,7 @@
         @endif
         {{ $label }}
     </dt>
-    <dd>
+    <dd style="text-align: {{ $align }} !important">
         @if ((!$slot->isEmpty()) && ($copy_what!=''))
             <x-copy-to-clipboard copy_what="{{ $copy_what }}">{{ $slot }}</x-copy-to-clipboard>
         @elseif (!$slot->isEmpty())
