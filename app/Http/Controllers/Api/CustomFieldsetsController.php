523920d6d6919c2db7b3ea1fdc3a1f2398a7bdb0
snipe <snipe@snipe.net>
2026-03-28 10:36:37 +0000

    Pint

--- app/Http/Controllers/Api/CustomFieldsetsController.php
+++ app/Http/Controllers/Api/CustomFieldsetsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\FilterRequest;
 use App\Http\Transformers\CustomFieldsetsTransformer;
 use App\Http\Transformers\CustomFieldsTransformer;
 use App\Models\CustomField;
