eb3a608e8056a1de6a80ecc169edce8a0aad20c5
snipe <snipe@snipe.net>
2026-04-03 12:27:50 +0100

    Bumped to pre version

--- config/version.php
+++ config/version.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'app_version' => 'v8.4.0',
-    'full_app_version' => 'v8.4.0 - build 21280-g91a95dbc6',
-    'build_version' => '21280',
+return array(
+    'app_version' => 'v8.4.1-pre',
+    'full_app_version' => 'v8.4.1-pre - build 22133-g6fbd189553',
+    'build_version' => '22133',
     'prerelease_version' => '',
-    'hash_version' => 'g91a95dbc6',
-    'full_hash' => 'v8.4.0-475-g91a95dbc6',
+    'hash_version' => 'g6fbd189553',
+    'full_hash' => 'v8.4.1-pre-851-g6fbd189553',
     'branch' => 'develop',
-];
+);
