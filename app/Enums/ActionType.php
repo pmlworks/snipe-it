bbe0c7409f382e9eebfc884e90b4fe4147d70ff0
snipe <snipe@snipe.net>
2026-04-09 14:48:22 +0100

    Added TokenRevoked action

--- app/Enums/ActionType.php
+++ app/Enums/ActionType.php
@@ -23,6 +23,8 @@ enum ActionType: string
     // Users
     case TwoFactorReset = '2FA reset';
     case Merged = 'merged';
+    case TokenRevoked = 'token revoked';
+    case TokenUnrevoked = 'token unrevoked';
 
     // Licenses
     case DeleteSeats = 'delete seats';
