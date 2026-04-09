a616da3e5c7e863e1d5c25d5ef3883abaeaa35ac
snipe <snipe@snipe.net>
2026-04-09 19:19:50 +0100

    Moved to an API-only header

---
+++ app/Http/Middleware/LogAuthedUserHeader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LogAuthedUserHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $response = $next($request);
+
+        if (config('app.authorized_user_header') === true) {
+            $response->headers->set('X-Authorized-User-ID', auth()?->id());
+        }
+        
+        return $response;
+    }
+}
