80f187ac7acd0aa1ea07979afd4881888829db61
Marcus Moore <contact@marcusmoore.io>
2026-05-05 13:26:08 -0700

    Scaffold tests

---
+++ tests/Feature/Reporting/Custom/CustomComponentReportTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Reporting\Custom;
+
+use Tests\TestCase;
+
+class CustomComponentReportTest extends TestCase
+{
+    public function test_requires_permission_to_view_page()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_requires_permission_to_run_report()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_can_load_custom_report_page()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_saved_templates_on_page_are_scoped_to_the_user_and_type()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_custom_accessory_report()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_custom_accessory_report_headers()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_custom_accessory_report_content()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_custom_accessory_report_exclude_deleted()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_custom_accessory_report_adheres_to_company_scoping()
+    {
+        $this->markTestIncomplete();
+    }
+}
