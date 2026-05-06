a84533b4f4b79c450020c68a6f8189b50cab380d
snipe <snipe@snipe.net>
2026-05-06 17:50:33 +0100

    Quick tweak to advanced search aliases

--- database/migrations/2026_05_05_125206_add_unique_index_to_nonces.php
+++ database/migrations/2026_05_05_125206_add_unique_index_to_nonces.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+        Artisan::call("saml:clear_expired_nonces");
         Schema::table('saml_nonces', function (Blueprint $table) {
             $table->dropIndex(['nonce']);
             $table->unique('nonce');
