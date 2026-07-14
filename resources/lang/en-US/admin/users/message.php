2beef8c763a4e7824cacbc34692cd052371bd5d0
snipe <snipe@snipe.net>
2026-07-14 14:25:21 +0100

    Added generic “Nothing checked out” string

--- resources/lang/en-US/admin/users/message.php
+++ resources/lang/en-US/admin/users/message.php
@@ -10,6 +10,7 @@ return [
     'user_not_found' => 'User does not exist or you do not have permission to view them.',
     'user_login_required' => 'The login field is required',
     'user_has_no_assets_assigned' => 'No assets currently assigned to user.',
+    'nothing_currently_assigned' => 'Nothing currently assigned.',
     'user_password_required' => 'The password is required.',
     'insufficient_permissions' => 'Insufficient Permissions.',
     'user_deleted_warning' => 'This user has been deleted. You will have to restore this user to edit them or assign them new assets.',
