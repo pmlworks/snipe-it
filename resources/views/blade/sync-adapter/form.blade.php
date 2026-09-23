0baf0a7238fad024b7026b8b1780efa1dfe05c89
snipe <snipe@snipe.net>
2026-09-23 18:16:25 +0100

    Render the HTML in the help text for baseUrl

--- resources/views/blade/sync-adapter/form.blade.php
+++ resources/views/blade/sync-adapter/form.blade.php
@@ -111,7 +111,7 @@
                 :label="trans('admin/settings/sync_adapters.base_url')"
                 :name="$urlField"
                 input_div_class="col-md-8"
-                :help_text="$adapter->baseUrlHelp() ?? trans('admin/settings/sync_adapters.base_url_help', ['type' => $adapter::typeLabel()])"
+                :help_html="$adapter->baseUrlHelp() ?? trans('admin/settings/sync_adapters.base_url_help', ['type' => $adapter::typeLabel()])"
                 required
             >
                 <x-slot:input>
