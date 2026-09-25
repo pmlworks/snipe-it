3d6e415efc3137d7ea7479d62d57a3be2cb57e86
snipe <snipe@snipe.net>
2026-09-25 22:47:38 +0100

    Switch to blade component for callout on checkout

--- resources/views/accessories/checkout.blade.php
+++ resources/views/accessories/checkout.blade.php
@@ -58,40 +58,7 @@
                 <div class="col-md-8 col-md-offset-3"><x-form.error name="checkout_qty" /></div>
             </div>
 
-            @if ($accessory->requireAcceptance() || $accessory->getEula() || ($snipeSettings->webhook_endpoint != ''))
-                <div class="form-group notification-callout">
-                    <div class="col-md-8 col-md-offset-3">
-                        <x-callout type="info" role="status">
-                            @if ($accessory->requireAcceptance())
-                                <i class="far fa-envelope fa-fw" aria-hidden="true"></i>
-                                {{ trans('admin/categories/general.required_acceptance') }}<br>
-                            @endif
-                            @if ($accessory->requireAcceptance() && (string) $snipeSettings->require_accept_signature === '1')
-                                <x-icon type="signature" class="fa-fw"/>
-                                {{ trans('admin/categories/general.required_signature') }}<br>
-                            @endif
-                            @if ($accessory->getEula())
-                                <i class="far fa-envelope fa-fw" aria-hidden="true"></i>
-                                {{ trans('admin/categories/general.required_eula') }}<br>
-                            @endif
-                            @if ($snipeSettings->webhook_endpoint != '')
-                                <i class="fab fa-slack fa-fw" aria-hidden="true"></i>
-                                {{ trans('general.webhook_msg_note') }}
-                            @endif
-                        </x-callout>
-                    </div>
-
-                    @if ($accessory->requireAcceptance())
-                        <div id="sign_in_place_div" class="col-md-7 col-md-offset-3">
-                            <label class="form-control">
-                                <input type="checkbox" value="1" name="sign_in_place" @checked(old('sign_in_place', session('sign_in_place', false))) aria-label="{{ trans('general.sign_in_place') }}">
-                                {{ trans('general.sign_in_place') }}
-                            </label>
-                            <p class="help-block">{{ trans('general.sign_in_place_help') }}</p>
-                        </div>
-                    @endif
-                </div>
-            @endif
+            <x-checkout.checkout-notification-callout :item="$accessory" :category="$accessory->category" />
 
             <x-form.row
                 :label="trans('admin/hardware/form.notes')"
